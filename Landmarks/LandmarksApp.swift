47a9c4437ca8f1dc094453ab7e1bade218a63dfa
Matt Gilbert <gh@mattgilbert.co>
2025-08-10 17:21:38 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Matt Gilbert on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
